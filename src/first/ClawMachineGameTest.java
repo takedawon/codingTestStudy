97d4a3593ce619076ab08b9dd5a2a2b0e8cbb757
takedawon <takedawon@naver.com>
2020-06-29 12:34:22 +0900

    [Level1] 2019 카카오 개발자 겨울 인턴십 크레인 인형뽑기 게임

---
+++ src/first/ClawMachineGameTest.java
@@ -0,0 +1,37 @@
+package first;
+
+import java.util.Stack;
+
+public class ClawMachineGameTest {
+    public static void main(String[] args) {
+        System.out.print(
+                solution(new int[][]{
+                                {0, 0, 0, 0, 0, 0},
+                                {0, 0, 1, 0, 3, 0},
+                                {0, 2, 5, 0, 1, 0},
+                                {4, 2, 4, 4, 2, 0},
+                                {3, 5, 1, 3, 1, 0}},
+                        new int[]{1, 5, 3, 5, 1, 2, 1, 4}));
+    } // 4 3 1 1 3 2 3
+    // 0
+    public static int solution(int[][] board, int[] moves) {
+        Stack<Integer> storageBox = new Stack();
+        int count = 0;
+        storageBox.push(0);
+        for (int move : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][move - 1] != 0) {
+                    if (storageBox.peek() == board[i][move - 1]) {
+                        count+=2;
+                        storageBox.pop();
+                    } else {
+                        storageBox.push(board[i][move - 1]);
+                    }
+                    board[i][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
